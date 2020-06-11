90cc5137ef39daf46d207859784e48f45b0e9f83
freakwho <freakwho@users.noreply.github.com>
2020-06-11 21:04:34 +0700

    Provider State Management
    Single Provider State Management
    Multi Provider State Management

---
+++ main.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutterprovider/application_color.dart';
+import 'package:flutterprovider/second.dart';
+import 'package:provider/provider.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: ChangeNotifierProvider<ApplicationColor>(
+        builder: (context) => ApplicationColor(),
+        child: MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Consumer<ApplicationColor>(
+          builder: (context, applicationColor, _) => Text(
+            "Single Provider State Management",
+            style: TextStyle(color: applicationColor.color),
+          ),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Consumer<ApplicationColor>(
+              builder:(context, applicationColor, _) => AnimatedContainer(
+                margin: EdgeInsets.all(5),
+                width: 100,
+                height: 100,
+                color: applicationColor.color,
+                duration: Duration(microseconds: 500),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(margin:EdgeInsets.all(5),child: Text("AB")),
+                Consumer<ApplicationColor>(
+                  builder:(context, applicationColor, _) => Switch(
+                    value: applicationColor.isLightBlue,
+                    onChanged: (newValue) {
+                      applicationColor.isLightBlue = newValue;
+                    },
+                  ),
+                ),
+                Container(margin:EdgeInsets.all(5),child: Text("LB")),
+              ],
+            ),
+            Container(
+              child: RaisedButton(
+                child: Text('Multi Provider'),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Second()),
+                  );// Navigate to second route when tapped.
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ money.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class Money with ChangeNotifier {
+  int _balance = 10000;
+
+  int get balance => _balance;
+  set balance(int value){
+    _balance = value;
+    notifyListeners();
+  }
+}
